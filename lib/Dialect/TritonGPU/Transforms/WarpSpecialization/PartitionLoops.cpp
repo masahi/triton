4d74aefed76d43af1d564521502744cd041740c9
Masahiro Masuda <masahi129@gmail.com>
2025-09-23 04:45:12 +0900

    wip

--- lib/Dialect/TritonGPU/Transforms/WarpSpecialization/PartitionLoops.cpp
+++ lib/Dialect/TritonGPU/Transforms/WarpSpecialization/PartitionLoops.cpp
@@ -184,10 +184,21 @@ void cloneOpsInBlock(Block *block, SmallVector<WarpGroupBuilder> &builders,
 
 void cloneForOp(scf::ForOp forOp, SmallVector<WarpGroupBuilder> &builders,
                 const PartitionSet &partitions) {
+  SetVector<int> forOpPartitions;
+  for (auto &op : forOp.getBody()->without_terminator()) {
+    auto opPartitions = triton::gpu::getPartitionIds(&op);
+    if (opPartitions) {
+      forOpPartitions.insert(opPartitions->begin(), opPartitions->end());
+    }
+  }
+
   SmallVector<scf::ForOp> newForOps;
-  for (auto [b, partition] : llvm::zip(builders, partitions.getPartitions())) {
+  for (int i: forOpPartitions) {
+    llvm::outs() << i << "\n";
+    auto b = builders[i];
+    auto partition = partitions.getPartition(i);
     auto [newLoopIndices, _] =
-        getLoopVarIndicesToKeep(forOp, &partition, partitions);
+        getLoopVarIndicesToKeep(forOp, partition, partitions);
     auto lb = b.mapping.lookupOrDefault(forOp.getLowerBound());
     auto ub = b.mapping.lookupOrDefault(forOp.getUpperBound());
     auto step = b.mapping.lookupOrDefault(forOp.getStep());
@@ -214,7 +225,7 @@ void cloneForOp(scf::ForOp forOp, SmallVector<WarpGroupBuilder> &builders,
 
   cloneOpsInBlock(forOp.getBody(), builders, partitions);
 
-  for (auto [i, newForOp] : enumerate(newForOps)) {
+  for (auto [i, newForOp] : llvm::zip(forOpPartitions, newForOps)) {
     builders[i].setInsertionPointAfter(newForOp);
     newForOp.walk([&](Operation *op) { op->removeAttr(kPartitionAttrName); });
     newForOp->removeAttr(kPartitionStagesAttrName);
@@ -367,6 +378,7 @@ void cloneOpsInBlock(Block *block, SmallVector<WarpGroupBuilder> &builders,
         }
       }
     } else {
+      op->dump();
       auto partitionIndices =
           getPartitionIds(op, partitions.getNumPartitions());
       cloneOp(op, builders, partitionIndices);
@@ -609,7 +621,7 @@ LogicalResult inferForOpPartitions(scf::ForOp forOp) {
     auto opPartitions = getPartitionIds(&op);
     forOpPartitions.insert(opPartitions->begin(), opPartitions->end());
   }
-  setPartition(forOp, forOpPartitions);
+  //  setPartition(forOp, forOpPartitions);
 
   SmallVector<SetVector<int>> iterArgsPartitions(forOp.getNumRegionIterArgs());
   for (auto [i, arg] : llvm::enumerate(forOp.getRegionIterArgs())) {
